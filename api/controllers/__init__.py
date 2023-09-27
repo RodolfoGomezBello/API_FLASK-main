15793169620961414bbfab72e4798283d2fc00e3
RodolfoGomezBello <yerb26@gmail.com>
2023-09-27 19:16:35 -0300

    Update __init__.py
    no se muestre pass

--- api/controllers/__init__.py
+++ api/controllers/__init__.py
@@ -8,7 +8,7 @@ MAIL_PORT = 587
 MAIL_USE_SSL = False
 MAIL_USE_TLS = True
 MAIL_USERNAME = 'socialchat2023@gmail.com'  # Tu dirección de correo electrónico
-MAIL_PASSWORD = 'mwrweqtioajjcwee'  # Tu contraseña de correo electrónico
+MAIL_PASSWORD = 'xxxxxxxxxxx'  # Tu contraseña de correo electrónico
 MAIL_DEFAULT_SENDER = 'socialchat2023@gmail.com'  # Tu dirección de correo electrónico predeterminada
 # Crea una instancia de Mail que será utilizada en los controladores
 mail = Mail()
